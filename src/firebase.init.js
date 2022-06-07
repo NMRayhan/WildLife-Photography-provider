5e36aa18a53615b29010569feb8a63a304d66592
NMRayhan <nurmdrayhan97@gmail.com>
2022-06-07 16:39:55 +0600

    Firebase init

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,17 +1,13 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDaNgvdDI8ltCgE99LcN8IoBTiqMFFalYU",
-  authDomain: "wild-life-photography-6822c.firebaseapp.com",
-  projectId: "wild-life-photography-6822c",
-  storageBucket: "wild-life-photography-6822c.appspot.com",
-  messagingSenderId: "758416591638",
-  appId: "1:758416591638:web:6c9407ad22833ab51e1e37",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
